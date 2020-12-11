f3fc4cce7f488052d5bfecf5d77d9766267162f9
Fiskmoz <fiskmoz96@gmail.com>
2020-12-11 12:06:05 +0100

    prod buold failing lol

--- front-end/src/app/components/inputs/typeahead/typeahead.component.ts
+++ front-end/src/app/components/inputs/typeahead/typeahead.component.ts
@@ -54,7 +54,7 @@ export class TypeaheadComponent implements OnInit {
     );
   };
 
-  selectedItem($event: any, input: any) {
+  selectedItem($event: any) {
     this.selected.emit($event["item"]);
   }
 
